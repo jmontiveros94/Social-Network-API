6f4d791e9507f9ebaeebc60869e78a68f2b16f40
Jacob <jm.ontiveros94@gmail.com>
2023-11-16 02:12:08 -0500

    added connection.js

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+// Imports mognoose and connect/connection
+const { connect, connection } = require('mongoose');
+
+// connection string to send to the server to run the database
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+
+// connects the databse using the string
+connect(connectionString);
+
+module.exports = connection;
