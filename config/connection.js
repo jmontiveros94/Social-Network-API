32b4deadda647a6272cd00895252ea18b992ff9d
Jacob <jm.ontiveros94@gmail.com>
2023-11-16 11:22:29 -0500

    adjusted connection.js file to accomodate localhost

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const { connect, connection } = require('mongoose');
 
 // connection string to send to the server to run the database
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1';
 
 // connects the databse using the string
 connect(connectionString);
